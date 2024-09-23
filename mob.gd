b9044171630b058f850f4819a06f220ec0c3cb3d
Robert Stancu <c.robertstancu@yahoo.com>
2024-09-23 22:10:59 +0300

    Changed mob script wrong name

--- mob.gd
+++ mob.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 
 func _ready() -> void:
-	var sprite_frames: SpriteFrames = $AnimatedSprite2D.sprite_frame
+	var sprite_frames: SpriteFrames = $AnimatedSprite2D.sprite_frames
 	var mob_types: Array = sprite_frames.get_animation_names()
 	$AnimatedSprite2D.play(mob_types[randi() % mob_types.size()])
 
